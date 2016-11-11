911c09b19b362300b8792d583abb4a70e667d925
Joseph Heller <joseph.heller.73@gmail.com>
2016-11-11 17:35:27 +0100

    First commit

---
+++ pru_loader.c
@@ -0,0 +1,58 @@
+#include "prussdrv.h"
+#include "pruss_intc_mapping.h"
+#include <stdint.h>
+
+int pru_init(void)
+{
+	tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
+
+	prussdrv_init();
+
+	if (prussdrv_open(PRU_EVTOUT_0))
+	{
+		return -1;
+	}
+	else
+	{
+		prussdrv_pruintc_init(&pruss_intc_initdata);
+		return 0;
+	}
+}
+
+void pru_load(int pru_num)
+{
+	/* write data from data.bin */
+	prussdrv_load_datafile(pru_num, DATA_FILE);				//DATA_FILE is defined in the build script
+
+	/* execute code on pru0 */
+	prussdrv_exec_program(pru_num, BIN_FILE);				//BIN_FILE is defined in the build script
+}
+
+void pru_stop(int pru_num)
+{
+	prussdrv_pru_disable(pru_num);
+	prussdrv_exit();	
+}
+
+int read_int(int32_t* x, size_t offset)
+{
+	volatile int32_t* p;
+
+	prussdrv_map_prumem(PRUSS0_SHARED_DATARAM, (void**)&p);
+
+	x[0] = p[2048 + offset];
+  
+	return 0;
+}
+
+
+int write_int(int32_t* x, size_t offset)
+{
+	volatile int32_t* p;
+
+	prussdrv_map_prumem(PRUSS0_SHARED_DATARAM, (void**)&p);
+
+	p[2048 + offset] = x[0];
+
+	return 0;
+}
---
+++ pru_loader.h
@@ -0,0 +1,10 @@
+#ifndef PRU_LOADER_NCLUDED
+#define PRU_LOADER_INCLUDED
+
+int pru_init(void);
+void pru_load(int);
+void pru_stop(int PRU_NUM);
+int read_int(int32_t*, size_t);
+int write_int(int32_t*, size_t);
+
+#endif /* PRU_LOADER_INCLUDED */
